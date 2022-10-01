a484994fd25c50d7cf1f5ed486ad587800e69d1e
Alonso Anchante Moreno <alonso.amoreno@live.com>
2022-10-01 09:15:37 -0500

    Inicio de proyecto

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <>
+            <header>
+                <h1>Planificador de Gastos</h1>
+            </header>
+        </>
+    )
+}
+
+export default Header;
