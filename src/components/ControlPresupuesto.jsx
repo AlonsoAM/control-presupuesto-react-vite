c0855b66c48f51035cafbf3a2a16d40d86499395
Alonso Anchante Moreno <alonso.amoreno@live.com>
2022-10-02 20:46:58 -0500

    Formatear a tipo moneda

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,4 +1,11 @@
 const ControlPresupuesto = ({ presupuesto }) => {
+  const formatearCantidad = (cantidad) => {
+    return cantidad.toLocaleString("es-PE", {
+      style: "currency",
+      currency: "PEN",
+    });
+  };
+
   return (
     <>
       <div className="contenedor-presupuesto contenedor sombra dos-columnas">
@@ -7,7 +14,13 @@ const ControlPresupuesto = ({ presupuesto }) => {
         </div>
         <div className="contenido-presupuesto">
           <p>
-            <span>Presupuesto: </span> S/. {presupuesto}
+            <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
+          </p>
+          <p>
+            <span>Disponible: </span> {formatearCantidad(0)}
+          </p>
+          <p>
+            <span>Gastado: </span> {formatearCantidad(0)}
           </p>
         </div>
       </div>
